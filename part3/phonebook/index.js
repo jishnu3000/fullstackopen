d78ed03a4b451b5ac45f8583bebe7d6ebfacf01b
Jishnu <71718745+jishnu3000@users.noreply.github.com>
2023-08-02 21:20:47 +0400

    exercise 3.9

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+
 const app = express()
 
 morgan.token('data', (req, res) => {
     return JSON.stringify(req.body)
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
@@ -117,7 +120,7 @@ app.post('/api/persons', (req, res) => {
     res.json(entry)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
