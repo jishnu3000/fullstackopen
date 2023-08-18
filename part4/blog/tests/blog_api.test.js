cfbec8f93df03c066d7b3451d163c178b4e8e046
Jishnu <71718745+jishnu3000@users.noreply.github.com>
2023-08-18 14:24:02 +0400

    exercise 4.9 and 4.10

--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -27,6 +27,35 @@ test('all blogs have returned', async () => {
     expect(response.body).toHaveLength(helper.initialBlogs.length)
 }, 100000)
 
+test('unique identifier property of the blog posts is named id', async () => {
+    const response = await api.get('/api/blogs')
+    const body = response.body
+
+    expect(body[0].id).toBeDefined()
+}, 100000)
+
+test('a new blog post can be created', async () => {
+    const newBlog = {
+        title: "Test Blog",
+        author: "Test author",
+        url: "www.testblog.com",
+        likes: 4,
+        __v: 0
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+    const titles = blogsAtEnd.map(blog => blog.title)
+    expect(titles).toContain('Test Blog')
+}, 100000)
+
 afterAll(async () => {
     await mongoose.connection.close()
 })
