735c6c6eca82a11cdfda901fdb21b8252467e3cd
Jishnu <71718745+jishnu3000@users.noreply.github.com>
2023-08-22 09:26:32 +0400

    exercise 4.21

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -44,8 +44,18 @@ blogsRouter.post('/', async (request, response, next) => {
 })
 
 blogsRouter.delete('/:id', async (request, response, next) => {
-    await Blog.findByIdAndRemove(request.params.id)
-    response.status(204).end()
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'invalid token'})
+    }
+
+    const blog = await Blog.findById(request.params.id)
+    if (blog.user.toString() === decodedToken.id.toString()) {
+        await Blog.findByIdAndRemove(request.params.id)
+        response.status(204).end()
+    } else {
+        return response.status(401).json({ error: 'blog access not authorized'})
+    }
 })
 
 blogsRouter.put('/:id', async (request, response, next) => {
