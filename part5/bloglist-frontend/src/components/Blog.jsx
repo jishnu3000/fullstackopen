f54245332ea680e412882ea6455cf55c000d501e
Jishnu <71718745+jishnu3000@users.noreply.github.com>
2023-08-25 15:43:00 +0400

    exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,42 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [show, setShow] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleShow = (event) => {
+    event.preventDefault()
+    setShow(!show)
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} <button onClick={toggleShow}>{ show ? 'hide' : 'view' }</button>
+      </div>
+      { show ? 
+        <div>
+          <div>
+            {blog.url}
+          </div>
+          <div>
+            likes {blog.likes} <button>like</button>
+          </div>
+          <div>
+            {blog.author}
+          </div>
+        </div> :
+        null
+      }
+    </div>
+  )
+}
 
 export default Blog
