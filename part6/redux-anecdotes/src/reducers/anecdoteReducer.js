e53da9a9f016248dabf418bb16a698bb9943eade
Jishnu <71718745+jishnu3000@users.noreply.github.com>
2023-08-29 21:00:07 +0400

    exercise 6.3

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -22,8 +22,15 @@ const initialState = anecdotesAtStart.map(asObject)
 const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
-
-  return state
+  switch (action.type) {
+    case 'VOTE':
+      const id  = action.payload.id
+      const votedAnecdote = state.find(anecdote => anecdote.id === id)
+      const changedAnecdote = { ...votedAnecdote, votes: votedAnecdote.votes + 1}
+      return state.map(anecdote => anecdote.id !== id ? anecdote : changedAnecdote)
+    default:
+      return state
+  }
 }
 
 export default reducer
