d5a07214bb1de7d3fd34c3daddbc85514b9f2924
Jishnu <71718745+jishnu3000@users.noreply.github.com>
2023-09-03 17:05:12 +0400

    Revert "initial country-hook"
    This reverts commit d860df59976604a37e8cbb4c428858cef98fc795.

--- part7/country-hook/src/components/SearchBar.js
+++
@@ -1,9 +0,0 @@
-const SearchBar = ({handleChange}) => {
-    return (
-        <div>
-            find countries <input onChange={handleChange} />
-        </div>
-    )
-}
-
-export default SearchBar
--- part7/country-hook/src/services/weather.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-const getWeather = (latitude, longitude, APIkey) => {
-    const request = axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${APIkey}&units=metric`)
-    return request.then(response => response.data)
-}
-
-export default { getWeather }
